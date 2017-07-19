cd0af8cebd21e00b6a22cd63ad60e737f7faa766
Elias Soares <elias_soares95@hotmail.com>
2017-07-19 20:26:50 -0300

    Criação método isMatrizQuadrada e mostraMatriz

--- matriz-int/Matriz.java
+++ matriz-int/Matriz.java
@@ -1,9 +1,33 @@
 
 public class Matriz
 {
-    private int matriz[][];
-    
+    //     private int matriz[][];
+    private int matriz[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+
     public Matriz(int matriz[][]) {
-    
+        this.matriz = matriz;
+    }
+
+    public static boolean isMatrizQuadrada(int matriz[][]) {        
+        boolean matrizQuadrada = true;
+        for (int linha = 0; linha < matriz.length; linha++) {
+            if (matriz.length != matriz[linha].length) {
+                matrizQuadrada = false;
+            }
+        }
+        return matrizQuadrada;
+    }
+
+    public static String mostraMatriz(int matriz[][]) {
+        String matrizFinal = "";
+
+        for (int linha = 0; linha < matriz.length; linha++) {
+            for (int coluna = 0; coluna < matriz.length; coluna++) {
+                matrizFinal += matriz[linha][coluna] + "  ";
+            } 
+            matrizFinal += "\n";
+        }
+
+        return matrizFinal;
     }
 }
---
+++ matriz-int/MatrizTeste.java
@@ -0,0 +1,8 @@
+
+public class MatrizTeste
+{
+    public static void main(String args[]) {
+        int m[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println(Matriz.mostraMatriz(m));
+    }
+}
