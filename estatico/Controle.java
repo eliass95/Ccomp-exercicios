bde4a45da7f312e2647d5c1402003f5aa62827a7
Elias Soares <elias_soares95@hotmail.com>
2017-08-03 19:17:56 -0300

    #estatico criação classe controle

---
+++ estatico/Controle.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Controle
+{
+    private ArrayList<Empregado> listEmp;
+    private ArrayList<Departamento> listDep;
+    
+    public Controle()
+    {
+        listEmp = new ArrayList<Empregado>();
+        listDep = new ArrayList<Departamento>();
+    }
+    
+    public void cadastraEmpregado(String nome, double salario) {
+        
+    }
+}
