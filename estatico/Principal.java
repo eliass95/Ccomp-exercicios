ea07052a7ef3bc476cca10ea1c6bc3a1639c3e14
Elias Soares <elias_soares95@hotmail.com>
2017-08-27 02:20:04 +0000

    refatoração classe Principal

--- estatico/Principal.java
+++ estatico/Principal.java
@@ -13,60 +13,71 @@ public class Principal
         while(op != 9) {
             showMenu();
             op = input.nextInt();
+            System.out.println("\n\n\n\n");
             switch(op) {
-                case 1:
-                    System.out.print("Nome: ");
+                case 1: // Cadastra Empregado
+                    System.out.print("CADASTRAR EMPREGADO> \nNome: ");
                     nome = input.next();
                     System.out.print("Salario: ");
                     salario = input.nextDouble();
                     controle.cadastraEmpregado(nome, salario);
                     break;
-                case 2:
-                    System.out.print(controle.listaEmpregado());
+                    
+                case 2: // Lista todos os Empregados
+                    System.out.print("EMPREGADOS> \n" + controle.listaEmpregado());
                     break;
-                case 3:
-                    System.out.print("Departamento: ");
+
+                case 3: // Cadastra Departamento
+                    System.out.print("CADASTRAR DEPARTAMENTO> \nDepartamento: ");
                     nome = input.next();
                     controle.cadastraDepartamento(nome);
                     break;
-                case 4:
-                    System.out.println("Adicionar Empregado ao departamento ----------");
+
+                case 4: // Adiciona Empregado ao Departamento
+                    System.out.println("ADICIONAR EMPREGADO AO DEPARTAMENTO>");
                     System.out.print("Matricula do empregado: ");
                     matricula = input.nextInt();
                     System.out.print("Departamento: ");
                     nome = input.next();
                     controle.adicionaEmpDep(nome, matricula);
                     break;
-                case 5:
-                    System.out.println("Remover Empregado do departamento ----------");
+
+                case 5: // Remove Empregado do Departamento
+                    System.out.println("REMOVER EMPREGADO DO DEPARTAMENTO>");
                     System.out.print("Matricula do empregado: ");
                     matricula = input.nextInt();
                     System.out.print("Departamento: ");
                     nome = input.next();
                     controle.removeEmpDep(nome, matricula);
                     break;
-                case 6:
-                    System.out.print("Departamento: ");
+
+                case 6: // Lista Empregados de um Departamento
+                    System.out.print("LISTAR EMPREGADOS> \nDepartamento: ");
                     nome = input.next();
                     System.out.println(controle.listaEmpDep(nome));
                     break;
-                case 7:
+
+                case 7: // Listar todos os Departamentos
+                    System.out.println("DEPARTAMENTOS>");
                     System.out.println(controle.listarDepartamentos());
                     break;
-                case 8:
-                    System.out.print("Excluir Departamento: ");
+
+                case 8: // Excluir Departamento
+                    System.out.print("EXCLUIR DEPARTAMENTO> \nDepartamento: ");
                     nome = input.next();
                     controle.excluirDepartamento(nome);
                     break;
-                case 9:
+
+                case 9: // Encerrar programa
                     System.out.print("Finalizando .  .  .  .  .  . ");
+                    input.close();
                     break;
             }
         }
     }
     
     public static void showMenu() {
-        String menu = "\n\n\nMenu ------------------------------ \n" +
+        String menu = "\n\n\n\n\n\n\n\n\n\nMenu ------------------------------ \n" +
                       "1 - Cadastrar Empregado \n" +
                       "2 - Listar todos Empregados \n" +
                       "3 - Cadastrar Departamento \n" +
